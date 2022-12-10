c931352e17e07b429c3dfbd07ab30490191c1481
Camilo Uribe <genkokun@gmail.com>
2022-12-09 22:07:35 -0500

    Commit

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+ScrollReveal().reveal('.top-banner');
+ScrollReveal().reveal('.nav-main');
 ScrollReveal().reveal('.banner');
 ScrollReveal().reveal('.content');
+ScrollReveal().reveal('.studies-content');
+ScrollReveal().reveal('.projects-content');
+ScrollReveal().reveal('.cards');
 ScrollReveal().reveal('.social');
